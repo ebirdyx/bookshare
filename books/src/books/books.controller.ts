160afc2a6c853e56a8d641ad2ee60ec12bebbc0b
Pamela Brahollari <ebirdyxx@gmail.com>
2021-11-25 20:56:12 +0100

    add book CRUD

--- books/src/books/books.controller.ts
+++ books/src/books/books.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { BooksService } from './books.service'
 import { Book } from "./book.interface";
 
 @Controller('books')
 export class BooksController {
-    constructor(private readonly bookService: BooksService) {}
+    constructor(private readonly bookService: BooksService) {
+    }
 
     @Get()
     public findAll(): Array<Book> {
         return this.bookService.findAll();
+    }
+
+    @Get(':id')
+    public findOne(@Param('id', ParseIntPipe) id: number): Book {
+        return this.bookService.findOne(id);
+    }
+
+    @Post()
+    public create(@Body() book: Book): Book {
+        return this.bookService.create(book);
+    }
+
+    @Delete(':id')
+    public delete(@Param('id', ParseIntPipe) id: number): void {
+        this.bookService.delete(id);
     }
 }
--- books/src/books/books.service.ts
+++ books/src/books/books.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { Book } from "./book.interface";
+
+const NOT_FOUND: number = -1;
 
 @Injectable()
 export class BooksService {
@@ -7,5 +9,40 @@ export class BooksService {
 
     public findAll(): Array<Book> {
         return this.books;
+    }
+
+    public findOne(id: number): Book {
+        const book: Book = this.books
+            .find(book => book.id === id);
+
+        if (book === null) {
+            throw new NotFoundException('Book not found.');
+        }
+
+        return book;
+    }
+
+    public create(book: Book): Book {
+        const titleExists: boolean = this.books
+            .some(item => item.title === book.title);
+
+        if (titleExists) {
+            throw new UnprocessableEntityException('Book title already exists.');
+        }
+
+        book.id = this.books.length;
+        this.books.push(book);
+        return book;
+    }
+
+    public delete(id: number): void {
+        const index: number = this.books
+            .findIndex(book => book.id === id);
+
+        if (index === NOT_FOUND) {
+            throw new NotFoundException('Book not found.');
+        }
+
+        this.books.splice(index, 1);
     }
 }
